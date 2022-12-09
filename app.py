c483e597b2eab4fa479200d3b44de36b880fcb08
isagajdak <isagajdak@jetlend.ru>
2022-12-09 03:06:14 +0200

    feat: auth

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
+from datetime import datetime, timedelta, timezone
+from functools import wraps
 import sqlite3
 import logging
+import jwt
 
 app = Flask(__name__)
 CORS(app)
 
+UNAUTHORIZED = 401
+NOT_FOUND = 404
+
+SECRET_FILE = 'secret.txt'
+HASH_ALGO = "HS256"
+with open(SECRET_FILE) as fr:
+    secret = fr.read().strip()
+
+LOG_FILE = 'history.log'
 logging.basicConfig(
-    filename='history.log',
+    filename=LOG_FILE,
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s',
     level=logging.DEBUG
@@ -15,35 +27,97 @@ logging.basicConfig(
 logger = logging.getLogger('only_logger')
 
 
-@app.route('/', methods=['GET'])
-def hello_world():
+def check_authorization(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if not token:
+            return jsonify(status=UNAUTHORIZED, message=["unauthorized"]), UNAUTHORIZED
+        try:
+            payload = jwt.decode(token, secret, algorithms=HASH_ALGO)
+        except jwt.exceptions.PyJWTError as e:
+            logger.warning(str(e))
+            return jsonify(status=UNAUTHORIZED, message=["unauthorized"]), UNAUTHORIZED
+        return f(*args, sub=payload['sub'], **kwargs)
+    return wrapper
+
+
+def get_auth_token(password: str) -> str:
+    current_time = datetime.now(tz=timezone.utc)
+    token = jwt.encode(
+        {
+            "sub": password,
+            "iat": current_time.timestamp(),
+            "exp": (current_time + timedelta(days=1)).timestamp()
+        },
+        secret,
+        algorithm=HASH_ALGO)
+    return token
+
+
+@app.route('/auth', methods=['POST'])
+def authenticate_user():
+    is_teacher = False
+    password = request.form.get('password')
     with sqlite3.connect("payments.sqlite") as con:
         cur = con.cursor()
         cur.execute("""
-            SELECT * FROM students;
-        """)
-        rows = cur.fetchall()
-    return jsonify(status=200, message=rows)
+                    SELECT firstname, middlename, lastname, grade, money, company FROM players WHERE password=?;
+                    """, (password,))
+        user = cur.fetchall()
+        if not user:
+            is_teacher = True
+            cur.execute("""
+                        SELECT firstname, middlename, lastname, money FROM teachers WHERE password=?;
+                        """, (password,))
+            user = cur.fetchall()
+        if not user:
+            return jsonify(status=UNAUTHORIZED, message=["wrong password"]), UNAUTHORIZED
+    token = get_auth_token(password)
+    return jsonify(status=200, message=user, teacher=is_teacher, auth_token=token)
 
 
-@app.route('/student', methods=['POST'])
-def register_student():
-
-    lastname = request.form.get('lastname')
-    firstname = request.form.get('firstname')
-    middlename = request.form.get('middlename')
-    email = request.form.get('email')
-    grade = int(request.form.get('grade'))
-
+@app.route('/debug', methods=['GET'])
+def debug_database():
     with sqlite3.connect("payments.sqlite") as con:
         cur = con.cursor()
         cur.execute("""
-            INSERT INTO students(firstname, middlename, lastname, grade, email, money, company)
-            VALUES (?, ?, ?, ?, ?, ?, ?) 
-            """, (firstname, middlename, lastname, grade, email, 0, None)
-        )
-        con.commit()
-    return jsonify(status=201, message="Student registered")
+                    SELECT * FROM players;
+                    """)
+        players = cur.fetchall()
+        cur.execute("""
+                    SELECT * FROM teachers;
+                    """)
+        teachers = cur.fetchall()
+    return jsonify(status=200, players=players, teachers=teachers)
+
+
+@app.route('/player', methods=['GET'])
+@check_authorization
+def get_player_info(sub=None):
+    with sqlite3.connect("payments.sqlite") as con:
+        cur = con.cursor()
+        cur.execute("""
+                    SELECT firstname, middlename, lastname, grade, money, company FROM players WHERE password=?;
+                    """, (sub,))
+        user = cur.fetchall()
+    if not user:
+        return jsonify(status=NOT_FOUND, message=["not a player"]), NOT_FOUND
+    return jsonify(status=200, player=user)
+
+
+@app.route('/teacher', methods=['GET'])
+@check_authorization
+def get_teacher_info(sub=None):
+    with sqlite3.connect("payments.sqlite") as con:
+        cur = con.cursor()
+        cur.execute("""
+                    SELECT firstname, middlename, lastname, money FROM teachers WHERE password=?;
+                    """, (sub,))
+        user = cur.fetchall()
+    if not user:
+        return jsonify(status=NOT_FOUND, message=["not a teacher"]), NOT_FOUND
+    return jsonify(status=200, teacher=user)
 
 
 if __name__ == '__main__':
--- create_db.py
+++ create_db.py
@@ -1,28 +1,78 @@
 import sqlite3
+import pandas as pd
+import numpy as np
+from hashlib import sha256
 
+IDS_FILE = 'ids.txt'
+TABLE_FILE = 'economic_game.xlsx'
 
 con = sqlite3.connect("payments.sqlite")
 cur = con.cursor()
 
-cur.execute("""CREATE TABLE students(student_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            firstname VARCHAR(255) NOT NULL,
-            middlename VARCHAR(255) NOT NULL,
-            lastname VARCHAR(255) NOT NULL,
-            grade INTEGER NOT NULL,
-            email VARCHAR(255) NOT NULL UNIQUE,
-            money INTEGER NOT NULL,
-            company INTEGER,
-            FOREIGN KEY (company) REFERENCES companies(company_id))""")
 
-cur.execute("""CREATE TABLE teachers(teacher_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            firstname VARCHAR(255) NOT NULL,
-            middlename VARCHAR(255) NOT NULL,
-            lastname VARCHAR(255) NOT NULL,
-            email VARCHAR(255) NOT NULL UNIQUE)""")
+def get_ids(from_file: str) -> set[str]:
+    with open(from_file) as fr:
+        return {line.strip() for line in fr.readlines()}
 
-cur.execute("""CREATE TABLE companies(company_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name VARCHAR(255) NOT NULL UNIQUE,
-            owner INTEGER NOT NULL,
-            FOREIGN KEY (owner) REFERENCES students(student_id))""")
 
-print('db created!')
+def assign_passwords(from_table: str, to_table: str, passwords: set[str]) -> pd.DataFrame:
+    df = pd.read_excel(from_table)
+    df['password'] = list(ids)[:len(df)]
+    df.sort_values(by='grade', inplace=True)
+    df.to_excel(to_table)
+    return df
+
+
+def create_tables() -> None:
+    cur.execute("""CREATE TABLE players(player_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    firstname VARCHAR(255) NOT NULL,
+                    middlename VARCHAR(255) NOT NULL,
+                    lastname VARCHAR(255) NOT NULL,
+                    grade INTEGER NOT NULL,
+                    password VARCHAR(255) NOT NULL UNIQUE,
+                    money INTEGER NOT NULL,
+                    company INTEGER,
+                    FOREIGN KEY (company) REFERENCES companies(company_id))""")
+
+    cur.execute("""CREATE TABLE teachers(teacher_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    firstname VARCHAR(255) NOT NULL,
+                    middlename VARCHAR(255) NOT NULL,
+                    lastname VARCHAR(255) NOT NULL,
+                    money INTEGER NOT NULL,
+                    password VARCHAR(255) NOT NULL UNIQUE)""")
+
+    cur.execute("""CREATE TABLE companies(company_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name VARCHAR(255) NOT NULL UNIQUE,
+                    money INTEGER NOT NULL,
+                    owner INTEGER NOT NULL,
+                    FOREIGN KEY (owner) REFERENCES players(player_id))""")
+    con.commit()
+
+
+def fill_in_tables(df: pd.DataFrame) -> None:
+    for _, row in df.iterrows():
+        hashed_pass = sha256(row['password'].encode()).hexdigest()
+        if row['grade'] is not np.nan:
+            cur.execute("""
+                        INSERT INTO players(firstname, middlename, lastname, grade, password, money, company)
+                        VALUES (?, ?, ?, ?, ?, ?, ?) 
+                        """, (row['firstname'], row['middlename'], row['lastname'], row['grade'], hashed_pass, 0, None)
+                        )
+        else:
+            cur.execute("""
+                        INSERT INTO teachers(firstname, middlename, lastname, password, money)
+                        VALUES (?, ?, ?, ?, ?) 
+                        """,
+                        (row['firstname'], row['middlename'], row['lastname'], hashed_pass, 0)
+                        )
+    con.commit()
+
+
+if __name__ == '__main__':
+
+    ids = get_ids(from_file=IDS_FILE)
+    table_name, extension = TABLE_FILE.rsplit('.', 1)
+    df = assign_passwords(from_table=TABLE_FILE, to_table=f'{table_name}_with_passwords.{extension}', passwords=ids)
+    create_tables()
+    fill_in_tables(df)
+    print('database created!')
