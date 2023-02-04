ac4aa2706edb400c7615dca98b8a6f2ab8ab4d06
FloppedWaffle <Hellers12372@gmail.com>
2023-02-04 15:27:30 +0300

    upd: bot

--- telegrambot/bot.py
+++ telegrambot/bot.py
@@ -2,20 +2,37 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from PIL import Image
+from os import environ
 
-bot = Bot(token="6158749782:AAG8pLkXKzV2ZPGbybpE8Dyo01YYYG3zN58")
+CHAT_ID = -1001758658558
+CHAT_SCHOOL_ID = -1001783533146
+token = environ.get("BOT_TOKEN")
+
+
+bot = Bot(token)
 dp = Dispatcher(bot)
 
-@dp.message_handler(content_types=types.ContentType.PHOTO)
-async def start_command(message: types.Message):
-    file_info = await bot.get_file(message.photo[-1].file_id)
-    await message.photo[-1].download(f"telegrambot/{file_info.file_path}")
-    
-    # photoW = round(message.photo[-1]["width"]*0.75)
-    # photoH = round(message.photo[-1]["height"]*0.75)
-    # with Image.open(f"telegrambot/{file_info.file_path}") as f:
-    #     f.resize((photoW, photoH), Image.LANCZOS)
-    #     f.save(f"telegrambot/{file_info.file_path}", quality=95, optimize=True)
+@dp.message_handler()
+async def private_reply(message: types.Message):
+    user_id = message.from_user.id
+
+    if message.text.lower() == "привет":
+        await bot.send_message(user_id, "И тебе привет ;p")
+    else:
+        await bot.send_message(user_id, 
+"""
+Бро, знаю, тебе очень хочется с кем-то пообщаться,
+но я не такой хороший собеседник, которым мог бы быть, 
+извини ;c
+
+@Твой тырильщик фотографий
+""")
+
+@dp.channel_post_handler(content_types=types.ContentType.PHOTO)
+async def photo_grab(message: types.Message):
+    if message.sender_chat.id == CHAT_ID:
+        file_info = await bot.get_file(message.photo[-1].file_id)
+        await message.photo[-1].download(destination_file=f"telegrambot/{file_info.file_path}")
 
 
 if __name__ == "__main__":
